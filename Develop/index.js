331479501eea3bec1797d9e1ca8b7e087732b623
Senfire <eric.mcghee@northwestern.edu>
2022-08-06 23:51:34 -0500

    Getting the readme formatting to work (Spacing matters)

--- Develop/index.js
+++ Develop/index.js
@@ -9,13 +9,19 @@ inquirer
   .prompt([
     {
       type: 'input',
+      message: 'Please enter the title of your project',
+      name: 'title',
+    },
+    {
+      type: 'input',
       message: 'describe your project:',
       name: 'description',
     },
     {
-      type: 'input',
+      type: 'checkbox',
       message: 'License information',
       name: 'license',
+      choices: ["MIT", "GPLv2", "Apache", "GLPv3", "BSD 3-clause", "Unlicense"],
 
     },
     {
@@ -25,7 +31,7 @@ inquirer
     },
     {
       type: 'input',
-      message: 'Usage:',
+      message: 'How do you use this prroject:',
       name: 'usage',
     },
     {
@@ -40,8 +46,8 @@ inquirer
     },
     {
      type: 'input',
-     message: 'description:',
-     name: 'test',
+     message: 'Who contributed to your project:',
+     name: 'contributors',
     },
 
   ])
@@ -65,41 +71,54 @@ inquirer
 
     }) =>
     `
-    ${testInstructions}
-    ${usage}
-    ${title}
+# ${title}
 
-
-    ## Description
+## Description
     ${description}
 
-    ## Table of contents
+- - - - -
 
-    ## Installation
+## Table of Contents
+
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributors](#contributors)
+[Test](#test)
+[Questions](#questions)
+
+## Description
+    ${description}
+
+## Installation
     ${installation}
 
-    ## Usage
+## Usage
     ${usage}
 
-    ## License
+## License
     This application is  covered under the [${license}] license.
 
-    ## Contributing
+## Contributors
     ${contributors}
 
-    ## Test
+## Tests
     ${test}
 
-    ## Questions
+## Questions
     ${gitHub}
     ${email}
+
+Please feel free to send questions via my GitHub profile:(https://github.com/${gitHub})
+Or email me via: ${email}
     `
-
+    // Collecting the data into a readme document varible
     let readmeDoc = readmeTemp(data)
-    const filename = `${data.description.toLowerCase().split(' ').join('')}.md`;
-
-    fs.writeFile(filename, JSON.stringify(readmeDoc, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
+    //formatting the title to be used as the file name (lowercase, remove spaces and combin the characters)
+    const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+    //Writing the file to the filesystem and adding the string template literal
+    fs.writeFile(filename, readmeDoc, null, (err) =>
+      err ? console.log(err) : console.log('ReadMe generated!')
     );
   });
 
